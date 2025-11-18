e2dce192ba3d85baae9cef70eb7f4abe940f7c0f
nishujangra <ndjangra1027@gmail.com>
2025-11-19 01:35:08 +0530

    initial xg cli setup

--- src/commands/init_handler.rs
+++ src/commands/init_handler.rs
@@ -1,11 +1,18 @@
-use crate::cli::init::InitArgs;
+use crate::cli::init::{InitArgs, Languages};
 
 pub fn handle_init(args: InitArgs) {
-    println!("🚀 Initializing project...");
-    println!("Language: {}", args.lang);
-    println!("Project Name: {}", args.project);
-    println!("REST Framework: {:?}", args.rest_framework);
+    match args.lang {
+        Languages::Go => {
+            println!("🚀 Initializing project...");
+            println!("Language: {:?}", args.lang);
+            println!("Project Name: {}", args.project);
+            println!("REST Framework: {:?}", args.rest_framework);
+            // Here you will call your generator logic later
+            println!("(Generator logic will go here)");
+        }
+        _ => {
+            println!("Wrong languages selected, {:?} is not currently supported", args.lang);
+        }
+    }
 
-    // Here you will call your generator logic later
-    println!("(Generator logic will go here)");
 }
